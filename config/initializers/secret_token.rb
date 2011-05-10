b8b5cdc52c7cb0267171e9c64ab7f143579276eb
vhariths <vedu.hariths@osumc.edu>
2011-05-10 13:22:02 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Practicethree::Application.config.secret_token = 'eb9843d1a0219ff2f1ef94ff601923450456298423c00617634452a448260ee7dfc6ce8f2a67c2107916a1e3b6037f22d3598ae7c59d6e83510f0140e8b0f2d7'
